874a690125e8a5f06dd2f802e263c0519e53f4d9
Mitchel Kelonye <kelonyemitchel@gmail.com>
2013-07-21 23:16:28 +0300

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='envaya',
-    version='0.0.1',
+    version='0.0.2',
     description='Tiny envaya utility',
     author='Mitchel Kelonye',
     author_email='kelonyemitchel@gmail.com',
     url='https://github.com/kelonye/django-envaya',
     requires=[
           'Django (>=1.3.0)'
-        , 'pytz (>=2013b)'
+        , 'pytz'
     ],
     packages=find_packages(),
     license='MIT License',
