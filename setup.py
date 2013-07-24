d8b097fe010ced095ef44745df93993e8d14b472
Mitchel Kelonye <kelonyemitchel@gmail.com>
2013-07-25 02:32:01 +0300

    Release 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='envaya',
-    version='0.0.3',
+    version='0.0.4',
     description='Tiny envaya utility',
     author='Mitchel Kelonye',
     author_email='kelonyemitchel@gmail.com',
